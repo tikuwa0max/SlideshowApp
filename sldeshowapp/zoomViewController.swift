28c0f265df54a07127bd6576204d9f3efab41340
市澤樹享 <tikuwa0max@ichizawajutoorunoMacBook-Air.local>
2018-11-29 13:15:19 +0900

    2画面目作成

---
+++ sldeshowapp/zoomViewController.swift
@@ -0,0 +1,40 @@
+//
+//  zoomViewController.swift
+//  sldeshowapp
+//
+//  Created by 市澤樹享 on 2018/11/29.
+//  Copyright © 2018 mikitaka.ichizawa. All rights reserved.
+//
+
+import UIKit
+
+class zoomViewController: UIViewController {
+
+    
+    @IBAction func back2(_ sender: Any) {
+        
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
