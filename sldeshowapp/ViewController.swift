50f08692db9026557e864813f6523083534501f0
市澤樹享 <tikuwa0max@ichizawajutoorunoMacBook-Air.local>
2018-11-29 13:07:46 +0900

    1画面目の完成

--- sldeshowapp/ViewController.swift
+++ sldeshowapp/ViewController.swift
@@ -12,21 +12,57 @@ class ViewController: UIViewController {
 
     @IBOutlet weak var imageboard: UIImageView!
 
+    @IBOutlet weak var nextbutton: UIButton!
+    @IBOutlet weak var backbutton: UIButton!
+    @IBOutlet weak var button: UIButton!
+    
     var imgs = [UIImage(named:"IMG_1298.jpg"),UIImage(named:"IMG_1302.jpg"),UIImage(named:"IMG_1305.jpg")]
     var count = 0
+    var timer:Timer!
     
     @IBAction func next(_ sender: UIButton) {
+        if timer == nil{
         count += 1
         if count == 3{
             count = 0
         }
+        
         imageboard.image = imgs[count]
-    }
+        }
+        }
     
     @IBAction func back(_ sender: UIButton) {
+        if timer == nil{
         count -= 1
         if count == -1{
             count = 2
+        }
+        imageboard.image = imgs[count]
+        }
+        }
+    
+    
+    @IBAction func startstop(_ sender: UIButton) {
+        if self.timer == nil {
+            self.timer = Timer.scheduledTimer(timeInterval: 2, target: self, selector: #selector(autochange(_:)), userInfo: nil, repeats: true)
+            button.setTitle("一時停止", for: .normal)
+            nextbutton.backgroundColor = UIColor(red: 1.0, green: 0.0, blue: 0.0, alpha: 0.2)
+            backbutton.backgroundColor = UIColor(red: 1.0, green: 0.0, blue: 0.0, alpha: 0.2)
+            }
+            
+        else {
+            self.timer.invalidate()
+            self.timer = nil
+            button.setTitle("再生", for: .normal)
+            nextbutton.backgroundColor = UIColor(red: 0.0, green: 0.0, blue: 0.0, alpha: 0.0)
+            backbutton.backgroundColor = UIColor(red: 0.0, green: 0.0, blue: 0.0, alpha: 0.0)
+            }
+        
+    }
+    @objc func autochange(_ timer: Timer){
+        count += 1
+        if count == 3{
+            count = 0
         }
         imageboard.image = imgs[count]
     }
