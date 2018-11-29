a5683d3e5d2c036a008cacbdcbc0caac27b6b259
市澤樹享 <tikuwa0max@ichizawajutoorunoMacBook-Air.local>
2018-11-29 12:57:03 +0900

    進む・戻るボタンの設置

--- sldeshowapp/ViewController.swift
+++ sldeshowapp/ViewController.swift
@@ -10,9 +10,32 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var imageboard: UIImageView!
+
+    var imgs = [UIImage(named:"IMG_1298.jpg"),UIImage(named:"IMG_1302.jpg"),UIImage(named:"IMG_1305.jpg")]
+    var count = 0
+    
+    @IBAction func next(_ sender: UIButton) {
+        count += 1
+        if count == 3{
+            count = 0
+        }
+        imageboard.image = imgs[count]
+    }
+    
+    @IBAction func back(_ sender: UIButton) {
+        count -= 1
+        if count == -1{
+            count = 2
+        }
+        imageboard.image = imgs[count]
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+    imageboard.image = imgs[count]
+        
     }
 
     override func didReceiveMemoryWarning() {
